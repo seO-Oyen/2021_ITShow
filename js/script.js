dce711fbab1b141be342b8d98fc24644d21937a1
chaewon0228 <s2033@e-mirim.hs.kr>
2021-05-30 22:24:14 +0900

    [Add] 게임 버튼 js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-(function(){
+(function () {
   const svg = document.querySelector(".confetti__image svg");
   const button = document.querySelector(".confetti__button");
   const isAnimationOk = window.matchMedia('(prefers-reduced-motion: no-preference)').matches;
 
   const colors = [
-    "#4b7bec", // blue
-    "#fc5c65", // red
-    "#fed330", // yellow
-    "#26de81", // green
-    "#2bcbba", // sea
-    "#fd9644", // orange
-    "#a55eea" // violet
+  "#4b7bec", // blue
+  "#fc5c65", // red
+  "#fed330", // yellow
+  "#26de81", // green
+  "#2bcbba", // sea
+  "#fd9644", // orange
+  "#a55eea" // violet
   ];
   const positions = [-200, -175, -150, -125, -100, -75, -50, 50, 75, 100, 125, 150, 175, 200];
   const shapes = [
-    "M -20 0 a 20 20 0 1 0 40 0 a 20 20 0 1 0 -40 0", // circle
-    "M -20 0 a 20 20 0 1 0 40 0 Z", // semicircle
-    "M -10 -10 H 20 V 20 H -10 Z", // square
-    "M 0 -15 L 20 20 L -20 20 Z" // triangle
+  "M -20 0 a 20 20 0 1 0 40 0 a 20 20 0 1 0 -40 0", // circle
+  "M -20 0 a 20 20 0 1 0 40 0 Z", // semicircle
+  "M -10 -10 H 20 V 20 H -10 Z", // square
+  "M 0 -15 L 20 20 L -20 20 Z" // triangle
   ];
   const shapeCount = 16;
   const buttonTl = gsap.timeline();
   gsap.set(button, {
-    transformOrigin: "50% 50%"
-  });
+    transformOrigin: "50% 50%" });
+
 
   buttonTl.to(button, {
     scale: 0.975,
     repeat: 1,
     yoyo: true,
     duration: 0.15,
-    ease: "Back.easeIn.config(1.5)"
-  });
+    ease: "Back.easeIn.config(1.5)" });
+
   buttonTl.pause();
 
   button.addEventListener("click", function (e) {
@@ -55,23 +55,23 @@
 
     gsap.set(elements, {
       transformOrigin: "50% 50%",
-      scale: "random(0.4, 1)",
-    });
+      scale: "random(0.4, 1)" });
+
     gsap.to(elements, {
       onComplete: removeElements,
       keyframes: [{
-        rotation: "random(-360, 360)",
+        rotate: "random(-360, 360)",
         x: `random([${positions.join(",")}])`,
         y: `random([${positions.join(",")}])`,
         ease: "expo.out",
         duration: 3,
         stagger: {
-          amount: 0.15
-        }
-      }, {
+          amount: 0.15 } },
+
+      {
         opacity: 0,
-        delay: -2.35
-      }]
-    });
+        delay: -2.35 }] });
+
+
   });
-}());
+})();
